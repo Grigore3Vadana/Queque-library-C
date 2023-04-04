08c503af53a6e4959cae53c9757cccd73555a883
Grigore Vadana <74039644+Grigore3Vadana@users.noreply.github.com>
2023-04-04 13:42:33 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "queque.h"
+
+int main()
+{
+    Queue *q = createQueue();
+
+    if (isEmpty(q))
+        printf("Coada este goala!\n");
+    else
+        printf("Coada nu este goala!\n");
+
+    enQueue(q, 1);
+    enQueue(q, 2);
+    enQueue(q, 3);
+    enQueue(q, 4);
+    enQueue(q, 5);
+
+    if (isEmpty(q))
+        printf("Coada este goala!\n");
+    else
+        printf("Coada nu este goala!\n");
+
+    while (!(isEmpty(q)))
+        printf("%d ", deQueue(q));
+    printf("\n");
+
+    if (isEmpty(q))
+        printf("Coada este goala!\n");
+    else
+        printf("Coada nu este goala!\n");
+
+    enQueue(q, 1);
+
+    if (isEmpty(q))
+        printf("Coada este goala!\n");
+    else
+        printf("Coada nu este goala!\n");
+
+    deleteQueue(q);
+
+    return 0;
+}
---
+++ queque.h
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef int Data;
+
+struct node
+{
+    Data val;
+    struct node *next;
+};
+typedef struct node Node;
+
+struct Q
+{
+    Node *front;
+    Node *rear;
+};
+typedef struct Q Queue;
+
+Queue *createQueue();
+void enQueue(Queue *, Data);
+Data deQueue(Queue *);
+bool isEmpty(Queue *);
+void deleteQueue(Queue *);
